77645b60940abb181186cdaf11071c63e5c9a7fe
ChangIn <qorckddls010@naver.com>
2018-08-02 22:52:22 +0900

    initial commit - 1

---
+++ django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/snow/Desktop/선린(개인)/UniFox/2019_unifox_website/django_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
